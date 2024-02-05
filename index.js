e9cd42634c9e5e51c86660356b7d0ddb0cac4b28
bishwarup307 <15094759+bishwarup307@users.noreply.github.com>
2024-02-05 23:50:23 +0530

    Create the grids using JS

---
+++ index.js
@@ -0,0 +1,30 @@
+const canvas = document.querySelector(".canvas");
+
+const N_ROW = 32;
+
+function getGridPixelDimension(canvas, numGrid) {
+  return parseInt(canvas.offsetWidth / numGrid);
+}
+
+function createGrids(canvas, numGrid) {
+  const placeHolder = document.createDocumentFragment();
+  gridDimension = getGridPixelDimension(canvas, N_ROW);
+
+  for (let i = 0; i < numGrid ** 2; i++) {
+    grid = document.createElement("div");
+    grid.setAttribute(
+      "style",
+      `
+        background-color: #e4e4e4;
+        border: 0.3px solid white;
+        width: ${gridDimension}px;
+        height: ${gridDimension}px
+        `
+    );
+    placeHolder.appendChild(grid);
+  }
+
+  canvas.appendChild(placeHolder);
+}
+
+createGrids(canvas, N_ROW);
