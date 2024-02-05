30c6582c82286f0af7cb28756b92edcbd0d74e7b
bishwarup307 <15094759+bishwarup307@users.noreply.github.com>
2024-02-06 00:35:11 +0530

    Add eventListener to create the grids from user's input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector(".canvas");
+const createGridBtn = document.querySelector("#btn-create-grid");
 
-const N_ROW = 32;
+// const N_ROW = 32;
 
 function getGridPixelDimension(canvas, numGrid) {
   return parseInt(canvas.offsetWidth / numGrid);
@@ -8,15 +9,15 @@ function getGridPixelDimension(canvas, numGrid) {
 
 function createGrids(canvas, numGrid) {
   const placeHolder = document.createDocumentFragment();
-  gridDimension = getGridPixelDimension(canvas, N_ROW);
+  gridDimension = getGridPixelDimension(canvas, numGrid);
 
   for (let i = 0; i < numGrid ** 2; i++) {
     grid = document.createElement("div");
     grid.setAttribute(
       "style",
       `
-        background-color: #e4e4e4;
-        border: 0.3px solid white;
+        background-color: #fff;
+        border: 0.3px solid #e4e4e4;
         width: ${gridDimension}px;
         height: ${gridDimension}px
         `
@@ -27,10 +28,19 @@ function createGrids(canvas, numGrid) {
   canvas.appendChild(placeHolder);
 }
 
-createGrids(canvas, N_ROW);
+createGridBtn.addEventListener("click", () => {
+  nGrid = parseInt(prompt("How many grids you want to create per row/column?"));
+
+  if (nGrid > 100 || nGrid < 4) {
+    alert("Please select a number between 4 and 100.");
+    return;
+  }
+
+  createGrids(canvas, nGrid);
+});
 
 canvas.addEventListener("mouseover", (event) => {
-  if (event.buttons === 2) event.target.style.backgroundColor = "";
+  // if (event.buttons === 2) event.target.style.backgroundColor = "";
 
   event.target.style.backgroundColor = "black";
 });
